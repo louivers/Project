ee61340a3f685e47e6f08910abf799ca7c226c8c
LynX-BE <niels.gaming1234@gmail.com>
2023-12-23 17:00:16 +0100

    reformatting oops

--- main.rs
+++ main.rs
@@ -1,10 +1,11 @@
-mod models;
 mod algorithms;
+mod models;
 
-use models::query;
 use algorithms::gyo;
+use models::query;
 
 fn main() {
+    // make vectors of variables which is a type of Term
     let r_atoms = vec![
         query::Term::Variable("x"),
         query::Term::Variable("y"),
@@ -15,9 +16,7 @@ fn main() {
         query::Term::Variable("b"),
         query::Term::Variable("x"),
     ];
-    let p_atoms = vec![
-        query::Term::Variable("b"),
-    ];
+    let p_atoms = vec![query::Term::Variable("b")];
     let my_body = vec![
         query::Atom {
             relation_name: "R",
@@ -30,11 +29,11 @@ fn main() {
         query::Atom {
             relation_name: "P",
             terms: p_atoms,
-        }
+        },
     ];
     let my_query = query::Query {
         head: vec!["x"],
-        body: my_body
+        body: my_body,
     };
     println!("{:#?}", gyo::gyo(&my_query));
 }
